9ec56520de3d2c67675f3f59258a249ede6be466
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 19:59:23 +0530

    feat: add register and login validation

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -4,14 +4,26 @@ import { asyncHandler } from "../../utils/asyncHandler.util.js";
 import { logger } from "../../utils/logger.util.js";
 import { RefreshToken } from "./RefreshToken.model.js";
 import { generateTokens } from "../../utils/generateToken.util.js";
+import { validateLogin, validateRegistration } from "./auth.validator.js";
 
 const userSignup = asyncHandler(async (req, res) => {
   logger.info("User registration endpoint hit");
+
+  const { error } = validateRegistration(req.body);
+  if (error) {
+    logger.warn(`Validation error: ${error.details[0].message}`);
+    return res
+      .status(400)
+      .json(new ApiResponse(400, {}, error.details[0].message));
+  }
+
   const { username, email, password } = req.body;
   let user = await User.findOne({ $or: [{ username }, { email }] });
   if (user) {
     logger.warn("User already exists");
-    return res.status(400).json(new ApiResponse(400, {}, "User already exists"));
+    return res
+      .status(400)
+      .json(new ApiResponse(400, {}, "User already exists"));
   }
 
   user = await User.create({
@@ -26,7 +38,7 @@ const userSignup = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-  }
+  };
 
   res
     .status(201)
@@ -35,45 +47,60 @@ const userSignup = asyncHandler(async (req, res) => {
     .json(
       new ApiResponse(
         201,
-        { user: {id: user._id, email, username} ,accessToken, refreshToken },
+        { user: { id: user._id, email, username }, accessToken, refreshToken },
         "User registered successfully",
       ),
     );
 });
 
-const userLogin = asyncHandler(async (req, res)=>{
-    logger.info("Login endpoint hit")
-    const {email, password} = req.body;
-    const existingUser = await User.findOne({email})
-    if(!existingUser){
-        logger.warn("User not found")
-        return res.status(404).json(new ApiResponse(404, {}, "User not found"))
-    }
+const userLogin = asyncHandler(async (req, res) => {
+  logger.info("Login endpoint hit");
 
-    const isPassValid = await user.checkPassword(password)
-    if(!isPassValid) {
-        logger.warn("Invalid password")
-        return res.status(400).json(new ApiResponse(400, {}, "Invalid Password"))
-    }
-
-    const { accessToken, refreshToken } = await generateTokens(user);
-
-    const user = {
-        id: existingUser._id,
-        username: existingUser.username,
-        email: existingUser.email
-    }
-
-    const options = {
-    httpOnly: true,
-    secure: true, // dont use in localhost it will break
+  const { error } = validateLogin(req.body);
+  if (error) {
+    logger.warn(`Validation error: ${error.details[0].message}`);
+    return res
+      .status(400)
+      .json(new ApiResponse(400, {}, error.details[0].message));
   }
 
-    res
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    logger.warn("User not found");
+    return res.status(404).json(new ApiResponse(404, {}, "User not found"));
+  }
+
+  const isPassValid = await existingUser.checkPassword(password);
+  if (!isPassValid) {
+    logger.warn("Invalid password");
+    return res.status(400).json(new ApiResponse(400, {}, "Invalid Password"));
+  }
+
+  const { accessToken, refreshToken } = await generateTokens(existingUser);
+
+  const user = {
+    id: existingUser._id,
+    username: existingUser.username,
+    email: existingUser.email,
+  };
+
+  const options = {
+    httpOnly: true,
+    secure: true, // dont use in localhost it will break
+  };
+
+  res
     .status(200)
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
-    .json(new ApiResponse(200, {user, accessToken, refreshToken}, "User Logged in successfully"))
-})
+    .json(
+      new ApiResponse(
+        200,
+        { user, accessToken, refreshToken },
+        "User Logged in successfully",
+      ),
+    );
+});
 
 export { userSignup, userLogin };
