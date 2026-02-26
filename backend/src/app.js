379deb280e30233c4ab978d65dd3bf119e2aec1f
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 17:38:54 +0530

    fix: forward async errors to express error handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express"
 import cors from "cors"
 import helmet from "helmet"
+import { errorHandler } from "./middlewares/error.middleware"
 
 const app = express()
 
@@ -8,5 +9,6 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(errorHandler)
 
 export default app
--- backend/src/utils/asyncHandler.util.js
+++ backend/src/utils/asyncHandler.util.js
@@ -1,5 +1,9 @@
+import logger from "../utils/logger.util.js"
+import { ApiResponse } from "./apiResponse.util.js";
+
 export const asyncHandler = (fn) => (req, res, next)=>{
     Promise.resolve(fn(req,res,next)).catch((err) => {
-      next(err);
+    logger.error(err);
+    next(err)
     });
 }
