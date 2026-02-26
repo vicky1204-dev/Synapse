0cff67d62eb819c72fd8e3aba1d3352b72f25a14
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 17:41:10 +0530

    refactor: remove unused imports from async handler

--- backend/src/utils/asyncHandler.util.js
+++ backend/src/utils/asyncHandler.util.js
@@ -1,5 +1,4 @@
 import logger from "../utils/logger.util.js"
-import { ApiResponse } from "./apiResponse.util.js";
 
 export const asyncHandler = (fn) => (req, res, next)=>{
     Promise.resolve(fn(req,res,next)).catch((err) => {
