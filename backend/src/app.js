65c1970da0e5d682779f5c0c6617f6f47a481f1c
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 18:44:47 +0530

    chore: configure cors and cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,29 @@ import express, { urlencoded } from "express"
 import cors from "cors"
 import helmet from "helmet"
 import { errorHandler } from "./middlewares/error.middleware"
+import cookieParser from "cookie-parser";
 
 const app = express()
 
 app.use(helmet())
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(cors({
+    origin: (origin, callback)=>{
+        const allowedOrigins = process.env.CORS_ORIGIN.split(",") || [];
+        if(!origin || !allowedOrigins.includes(origin)){
+            callback(null, true)
+        } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    allowedHeaders: ["Content-Type", "Authorization", "Accept-Version"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true // you cant use * as origin if credentials are true
+}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(cookieParser())
+
+
 app.use(errorHandler)
 
 export default app
