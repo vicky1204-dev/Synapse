6549ace2b483923e9ea7580348c32e0924b4c83d
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 17:36:22 +0530

    feat: add error handler middleware

---
+++ backend/src/middlewares/error.middleware.js
@@ -0,0 +1,8 @@
+import { ApiResponse } from "../utils/apiResponse.util.js";
+import { logger } from "../utils/logger.util.js";
+
+export const errorHandler = (err, req, res, next) => {
+    logger.error(err.stack)
+
+     res.status(err.status || 500).json(new ApiResponse(500, {}, "Internal Server Error"))
+}
