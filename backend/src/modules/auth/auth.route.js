d02aac4c30f2ab979fcf9572964b06c60121526d
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 19:42:13 +0530

    feat: add login route to express router

--- backend/src/modules/auth/auth.route.js
+++ backend/src/modules/auth/auth.route.js
@@ -1,8 +1,9 @@
-import { userSignup } from "./auth.controller.js";
+import { userSignup, userLogin } from "./auth.controller.js";
 import { Router } from "express";
 
 const router = Router()
 
 router.post("/register", userSignup)
+router.post("/login", userLogin)
 
 export default router
