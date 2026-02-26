3a0ef4b670cfa76a4b363fec5d3fcd22cf3f21ba
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-26 18:56:51 +0530

    feat: add signup route to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(helmet())
 app.use(cors({
     origin: (origin, callback)=>{
-        const allowedOrigins = process.env.CORS_ORIGIN.split(",") || [];
+        const allowedOrigins = process.env.CORS_ORIGIN.split(",");
         if(!origin || !allowedOrigins.includes(origin)){
             callback(null, true)
         } else {
@@ -23,6 +23,10 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser())
+
+
+import authRouter from "./modules/auth/auth.route.js"
+app.use("/api/auth", authRouter)
 
 
 app.use(errorHandler)
