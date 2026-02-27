4fcadf452a513a60989e60cdf45318817b9c03b8
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-27 20:00:09 +0530

    feat: implement AuthContext and useAuth hook for user authentication management

---
+++ frontend/src/auth/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useState } from "react";
+import { getCurrentUser } from "./auth.service.js";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadUser() {
+      try {
+        const res = await getCurrentUser();
+        setUser(res.data.data);
+      } catch {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadUser();
+  }, []);
+
+  return (
+    <AuthContext.provider value={{ user, setUser, loading }}>
+      {children}
+    </AuthContext.provider>
+  );
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./auth/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
     <App />
-  </StrictMode>,
-)
+  </AuthProvider>,
+);
