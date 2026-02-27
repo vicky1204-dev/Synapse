b6e7a5a9afe90ef280d1cb42b02866256f6186e1
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-02-27 19:24:51 +0530

    feat: implement authentication service with login, signup, logout, and get current user functions

---
+++ frontend/src/auth/auth.service.js
@@ -0,0 +1,6 @@
+import { api } from "../utils/axios.js";
+
+export const login = (data) => api.post("/auth/login", data)
+export const signup = (data) => api.post("/auth/register", data)
+export const logout = () => api.post("/auth/logout")
+export const getCurrentUser = () => api.post("/users/get-user")
