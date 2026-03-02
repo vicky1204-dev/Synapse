0d415251113544009cf5058cf3196263b8d47342
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-03-02 20:57:44 +0530

    feat: implement MainLayout with SideBar and enhance SideBar with dynamic links and animations

---
+++ frontend/src/components/MainLayout.jsx
@@ -0,0 +1,68 @@
+import { motion, AnimatePresence, usePresence } from "framer-motion";
+import SideBar from "./SideBar";
+import { useLocation, useOutlet } from "react-router-dom";
+import { useRef, useLayoutEffect, useState } from "react";
+
+//Use useLayoutEffect (not useEffect) so measurement happens before paint:
+
+const MainLayout = () => {
+  const location = useLocation();
+  const outlet = useOutlet();
+  const contentRef = useRef(null);
+  const [contentLeft, setContentLeft] = useState(0);
+  useLayoutEffect(() => {
+    if (contentRef.current) {
+      const rect = contentRef.current.getBoundingClientRect();
+      setContentLeft(rect.left);
+    }
+  }, []);
+
+  return (
+    <div className="min-h-screen w-full flex">
+      <div className="sm:sticky fixed left-0 top-0 h-screen flex-[0,0,auto] sm:py-8 sm:pl-8 z-50 opacity-0 -translate-y-full sm:translate-0 sm:opacity-100">
+        <SideBar />
+      </div>
+      <div className="flex-1 relative min-h-screen bg-white/15">
+        <AnimatePresence mode="wait">
+          //since i am animating 2 elements and animatepresence expects only one
+          direct child so wrap them with div with key
+          <motion.div key={location.pathname}>
+            {/* SLIDER ******** */}
+            <motion.div
+              className="fixed inset-y-0 right-0 bg-black z-40 pointer-events-none"
+              style={{
+                left: contentLeft,
+              }}
+              initial={{ y: "100%" }}
+              animate={{ y: "100%" }}
+              exit={{ y: "0%" }}
+              transition={{ duration: 0.8, ease: [0.76, 0, 0.24, 1] }}
+            />
+
+            {/* PAGE */}
+            <motion.main
+              initial={{ opacity: 1, scale: 1, y: 0 }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+                y: 0,
+              }}
+              exit={{
+                opacity: 0.5,
+                scale: 0.9,
+                y: -100,
+                transition: { duration: 0.9, ease: [0.76, 0, 0.24, 1] },
+              }}
+              ref={contentRef}
+              className="relative min-h-screen"
+            >
+              {outlet}
+            </motion.main>
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,12 +1,182 @@
-import React from 'react'
+import {
+  CircleQuestionMark,
+  Notebook,
+  MessagesSquare,
+  MessageCircle,
+  NotepadText,
+  TvMinimalPlay,
+  AudioLines,
+} from "lucide-react";
+import { Link, NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const sideBarData = [
+  {
+    category: "Study",
+    links: [
+      {
+        icon: CircleQuestionMark,
+        title: "Questions",
+        meta: "Ask your doubts",
+        to: "/study/questions",
+      },
+      {
+        icon: Notebook,
+        title: "Notes",
+        meta: "Get materials",
+        to: "/study/notes",
+      },
+      {
+        icon: MessagesSquare,
+        title: "Discussions",
+        meta: "Discuss in groups",
+        to: "/study/discussions",
+      },
+    ],
+  },
+  {
+    category: "Personal",
+    links: [
+      {
+        icon: MessageCircle,
+        title: "Chats",
+        meta: "Your Chats",
+        to: "/personal/chats",
+      },
+      {
+        icon: CircleQuestionMark,
+        title: "Questions",
+        meta: "Your Questions",
+        to: "/personal/my-questions",
+      },
+      {
+        icon: NotepadText,
+        title: "Notes",
+        meta: "Your Notes",
+        to: "/personal/my-notes",
+      },
+    ],
+  },
+  {
+    category: "Hangout",
+    links: [
+      {
+        icon: TvMinimalPlay,
+        title: "Watch2gether",
+        meta: "Watch YT videos",
+        to: "/coming-soon",
+      },
+      {
+        icon: AudioLines,
+        title: "Listen2gether",
+        meta: "Listen songs",
+        to: "/coming-soon",
+      },
+    ],
+  },
+];
 
 const SideBar = () => {
   return (
     <>
-    <div>Sidebar</div>
+      <motion.div
+        initial={{ y: 20, opcaity: 0 }}
+        animate={{ y: 0, opacity: 1, transition: { duration: 0.3 } }}
+        className="h-full flex flex-col justify-between py-8 px-4 border border-white/20 rounded-lg bg-black"
+      >
+        <Link to="/" className="flex items-center justify-center">
+          <motion.img
+            initial={{ opacity: 0, filter: "blur(12px)" }}
+            animate={{
+              opacity: 1,
+              filter: "blur(0px)",
+              transition: { duration: 0.6 },
+            }}
+            src="/assets/synapse_logo.svg"
+            alt=""
+          />
+        </Link>
 
+        {sideBarData.map((data, index) => {
+          return (
+            <div key={data.category} className="flex flex-col gap-2">
+              <motion.h1
+                initial={{ y: 20, opcaity: 0 }}
+                animate={{ y: 0, opacity: 1, transition: { duration: 0.3 } }}
+                className="text-xs text-text-secondary ml-3"
+              >
+                {data.category}
+              </motion.h1>
+              <div className={`flex flex-col gap-0 `}>
+                {data.links.map((item, index) => {
+                  const IconComponent = item.icon;
+
+                  return (
+                    <NavLink to={item.to} key={item.to} className={`relative`}>
+                      {({ isActive }) => {
+                        return (
+                          <motion.div
+                            initial={{
+                              opacity: 0,
+                              y: 10,
+                              filter: "blur(12px)",
+                            }}
+                            animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+                            transition={{
+                              duration: 0.6,
+                              delay: index * 0.1,
+                              ease: [0.22, 1, 0.36, 1],
+                            }}
+                            className={`relative flex items-center justify-start gap-2 rounded-lg py-2 px-3 ${
+                              isActive ? "" : "hover:bg-bg-secondary hover:z-0"
+                            }`}
+                          >
+                            {isActive && (
+                              <motion.div
+                                layoutId={data.category}
+                                transition={{
+                                  duration: 0.2,
+                                  ease: "easeInOut",
+                                }}
+                                className="bg-white rounded-lg inset-0 z-4 absolute pointer-events-none"
+                              ></motion.div>
+                            )}
+                            <div
+                              className={`p-2 rounded-sm border w-fit h-fit z-10 mix-blend-difference border-white/20 text-text-secondary`}
+                            >
+                              <IconComponent strokeWidth={1} size={18} />
+                            </div>
+
+                            <div className="flex flex-col items-start justify-center z-10">
+                              <h1
+                                className={
+                                  isActive ? "text-black" : "text-white"
+                                }
+                              >
+                                {item.title}
+                              </h1>
+
+                              <span className="text-xs text-text-secondary mix-blend-difference">
+                                {item.meta}
+                              </span>
+                            </div>
+                          </motion.div>
+                        );
+                      }}
+                    </NavLink>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+        <div className="flex items-center justify-center flex-col">
+          <button>Logout</button>
+          <Link>Profile</Link>
+        </div>
+      </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
