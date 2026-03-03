a9c0d899758a1e89bad8f1f6141d6544c53c6ad5
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-03-03 12:11:43 +0530

    feat: add refresh-access-token route

--- backend/src/modules/auth/auth.route.js
+++ backend/src/modules/auth/auth.route.js
@@ -1,9 +1,10 @@
-import { userSignup, userLogin } from "./auth.controller.js";
+import { userSignup, userLogin, refreshAccessToken } from "./auth.controller.js";
 import { Router } from "express";
 
 const router = Router()
 
 router.post("/register", userSignup)
 router.post("/login", userLogin)
+router.post("/refresh-access-token", refreshAccessToken)
 
 export default router
