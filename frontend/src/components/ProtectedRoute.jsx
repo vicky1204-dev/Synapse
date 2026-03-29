ac809108c8318150c5d983483ab42d1cdbbb460d
Ashutosh Samal <samalashutosh1512@outlook.com>
2026-03-29 17:25:22 +0530

    feat: enhance ProtectedRoute component with loading state and animated login prompt

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,13 +1,35 @@
-import {useAuth} from "../auth/useAuth.js"
+import { motion } from "framer-motion";
+import { useAuth } from "../auth/useAuth.js";
+import { useNavigate } from "react-router-dom";
+import { Lock } from "lucide-react";
 
-const ProtectedRoute = ({children}) => {
-  const {user, loading} = useAuth()
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate()
 
   return (
-  <>
-  {loading ? <div>...loading</div> : !user ? <div>Log In Please</div> : children}
-  </>
-  )
-}
+    <>
+      {loading ? (
+        <div>...loading</div>
+      ) : !user ? (
+        <div className="bg-black p-8 w-full min-h-screen flex gap-4 items-center justify-center">
+          <motion.h1
+            initial={{ opacity: 0, filter: "blur(12px)" }}
+            animate={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            className="text-3xl font-light flex gap-2"
+          >
+            <span className="font-medium hover:cursor-pointer hover:text-neutral-400 duration-150 flex items-center gap-2" onClick={()=> navigate("/login")}><Lock size={32}/> Login</span> to continue
+          </motion.h1>
+        </div>
+      ) : (
+        children
+      )}
+    </>
+  );
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
